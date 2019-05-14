5d8641ddd78d04b42b334dc5325e67645a8949d2
Kabiswa Davis <kabiswad@gmail.com>
2019-05-14 03:52:21 +0300

    add signup form

--- UI/login.js
+++ UI/login.js
@@ -1,7 +1,17 @@
 function openForm() {
-  document.getElementById("myForm").style.display = "block";
+ 	document.getElementById("myForm").style.display = "block";
+ 	document.getElementById("id001").style.display = "none";
 }
 
 function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+ 	document.getElementById("myForm").style.display = "none";
+
+}
+function openSignup() {
+	  document.getElementById("id001").style.display = "block";
+	  document.getElementById("myForm").style.display = "none";
+}
+function closeSignup() {
+ 	document.getElementById("id001").style.display = "none";
+
 }
