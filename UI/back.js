5a901d07777c06701baef3bd0a1e561ff3b5cfa9
Kabiswa Davis <kabiswad@gmail.com>
2019-05-19 23:20:00 +0300

    Add responsiveness to the project

--- UI/back.js
+++ UI/back.js
@@ -1,3 +1,14 @@
 function goBack() {
 	window.history.back();
 }
+let y =document.getElementById('ul1');
+let x =document.getElementById('i1');
+
+function openMenu() {
+	if(y.style.display === "block"){
+		y.style.display = "none";
+	}
+	else {
+		y.style.display = "block";
+	}
+}
