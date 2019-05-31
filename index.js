f1743d69d4899883409167401e841a8b99fbf444
Kabiswa Davis <kabiswad@gmail.com>
2019-06-01 00:44:58 +0300

    Create test folder

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`magic happens on port ${port}`));
+
+module.exports = port;
