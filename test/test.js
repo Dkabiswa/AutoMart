87cc504256de051d3a7d0a70dfb39ae0f36bd270
Kabiswa Davis <kabiswad@gmail.com>
2019-06-01 10:45:46 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ const server = require('../index');
 
 describe('test', () => {
   it('should return a string', () => {
-    expect('ci with travis').to.equal('ci with travis');
+    expect('Hello').to.equal('Hello');
   });
 });
