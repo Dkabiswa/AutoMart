a8a03461bf655f552635c2427dcc233d08d3d4e4
Kabiswa Davis <kabiswad@gmail.com>
2019-06-07 03:45:13 +0300

    Write tests for verifying jwt token middleware

--- src/auth/local.js
+++ src/auth/local.js
@@ -1,18 +1,36 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
-require('dotenv').config();
+dotenv.config();
 
 const Auth = {
-  createToken(id) {
+  createToken({ id }) {
     const expiresIn = 24 * 60 * 60;
     return jwt.sign({ id }, process.env.SECRET_KEY, {
       expiresIn,
     });
   },
 
-  verifyToken() {
+  verifyUser(req, res, next) {
+  try {
+    const header = req.headers.authorization;
+    if (header === undefined) return res.status(401).json({ 
+    	status: 401, 
+    	error: 'Unauthorized' 
+    });
 
-  },
+    req.user = jwt.verify(header, process.env.SECRET_KEY);
+    next();
+    } catch {
+    return res.status(401).json({ 
+    	status: 401, 
+    	error: 'Invalid token!' 
+    });
+  }
+  return false;
+},
 
-};
+}
+
+
 export default Auth;
