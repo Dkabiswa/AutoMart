d44785cd9f98e808151a5fd952d5c5b04036ee63
Kabiswa Davis <kabiswad@gmail.com>
2019-06-15 14:11:25 +0300

    Handle 405 method not allowed for home route

--- server/dist/index.js
+++ server/dist/index.js
@@ -9,13 +9,13 @@ var _express = _interopRequireDefault(require("express"));
 
 var _bodyParser = _interopRequireDefault(require("body-parser"));
 
-var _expressValidator = _interopRequireDefault(require("express-validator"));
-
 var _carRoutes = _interopRequireDefault(require("./src/routes/carRoutes"));
 
 var _userRoutes = _interopRequireDefault(require("./src/routes/userRoutes"));
 
 var _orderRoutes = _interopRequireDefault(require("./src/routes/orderRoutes"));
+
+var _methods = _interopRequireDefault(require("./src/middleware/methods"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -24,7 +24,14 @@ app.use(_bodyParser["default"].urlencoded({
   extended: false
 }));
 app.use(_bodyParser["default"].json());
-app.use((0, _expressValidator["default"])());
+var route = '/';
+app.get(route, function (req, res) {
+  res.status(200).json({
+    status: 200,
+    message: 'WELCOME, THIS IS AUTOMART'
+  });
+});
+app.all(route, _methods["default"]);
 app.use('/api/v1', _carRoutes["default"]);
 app.use('/api/v1/auth', _userRoutes["default"]);
 app.use('/api/v1', _orderRoutes["default"]);
