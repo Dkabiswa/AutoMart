ebe3f1104c8a73d439f151b589277d713cef356e
Kabiswa Davis <kabiswad@gmail.com>
2019-06-15 20:53:04 +0300

    Set up image upload using multer

--- server/index.js
+++ server/index.js
@@ -4,8 +4,18 @@ import car from './src/routes/carRoutes';
 import user from './src/routes/userRoutes';
 import order from './src/routes/orderRoutes';
 import method from './src/middleware/methods';
+import upload from './src/middleware/multer';
+
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.post('/upload', upload.single('image'), (req, res) => {
+  console.log('Should be undefined:', req.file);
+  res.send('OK')
+})
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -18,6 +28,8 @@ app.get(route, (req, res) => {
   });
 });
 app.all(route, method);
+
+
 
 app.use('/api/v1', car);
 app.use('/api/v1/auth', user);
