fd023c9629c194c90d45f5ceb28a3ab6be802959
Kabiswa Davis <kabiswad@gmail.com>
2019-06-17 22:26:07 +0300

    Store User password as a hash

--- server/src/dBase/controllers/userController.js
+++ server/src/dBase/controllers/userController.js
@@ -3,6 +3,7 @@ import auth from '../../middleware/auth';
 import UserSchema from '../../validations/userValidation';
 import Validation from '../../middleware/validationhandler';
 import db from '../db/dbControl';
+import bcrypt from 'bcrypt';
 import '@babel/polyfill';
 
 
@@ -15,7 +16,7 @@ const User = {
     const { email } = req.body;
     const last_name = req.body.lastName;
     const first_name = req.body.firstName;
-    const { password } = req.body;
+    const  password  = const hash = bcrypt.hashSync(req.body.password, 10);
     const { address } = req.body;
     const is_admin = req.body.isAdmin;
     const query = `INSERT INTO
